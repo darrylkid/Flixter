a89373d6f0a48de6cf904a47adc586a0dc3caaed
darrylkid <darrylbaailey@gmail.com>
2021-06-25 16:48:49 -0700

    Add 2 gifs to demonstrate Flixter - one portrait and one landscape.

--- app/src/main/java/com/example/flixter/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixter/MovieDetailsActivity.java
@@ -2,11 +2,14 @@ package com.example.flixter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.flixter.models.Movie;
 
 import org.parceler.Parcels;
@@ -19,6 +22,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvDetailTitle;
     TextView tvDetailOverview;
     RatingBar rbVoteAverage;
+    ImageView ivDetailPoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvDetailTitle = (TextView) findViewById(R.id.tvDetailTitle);
         tvDetailOverview = (TextView) findViewById(R.id.tvDetailOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivDetailPoster = (ImageView) findViewById(R.id.ivDetailPoster);
 
         // Unwrap the parcel
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
@@ -43,5 +48,23 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // 0 - 5.
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage / 2.0f);
+
+        String imageUrl;
+
+        // Image-related view
+        // if the phone orientation is in landscape
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // then use the background image
+            imageUrl = movie.getBackDropPath();
+        } else {
+            // else use the poster image
+            imageUrl = movie.getPosterPath();
+        }
+
+        // Show the placeholder image if the image has not fully loaded.
+        Glide.with(this)
+                .load(imageUrl)
+                .placeholder(R.drawable.placeholder)
+                .into(ivDetailPoster);
     }
 }
